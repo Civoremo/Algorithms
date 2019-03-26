73ece03f16fe56614cf6ad2700ea6ddd53cda3b0
Civoremo <omerovic_n@hotmail.com>
2019-03-26 15:31:16 -0400

    making_change using polynomial to solve

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -5,13 +5,16 @@ import sys
 
 def making_change(amount, denominations):
     # pass
-    if amount < 0:
-        return 0
-    if amount == 0:
-        return 1
-    if not denominations:
-        return 0
-    return making_change(amount-denominations[0], denominations) + making_change(amount, denominations[1:])
+    # if amount < 0:
+    #     return 0
+    # if amount == 0:
+    #     return 1
+    # if not denominations:
+    #     return 0
+    # return making_change(amount-denominations[0], denominations) + making_change(amount, denominations[1:])
+
+    amount = amount/100
+    return round(1 + (55*amount + 238*amount**2 + 380*amount**3 + 200*amount**4)/3)
 
     # cache = [0 for num in range(amount+1)]
     # cache[0] = 1
