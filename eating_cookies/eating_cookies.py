a20357be5f80cf388860bb950899b3a320c640cb
Civoremo <omerovic_n@hotmail.com>
2019-03-26 15:23:20 -0400

    making_change small tests pass

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,19 +8,28 @@ import sys
 
 
 def eating_cookies(n, cache=None):
-    # pass
-    # if n == 1 or n == 0:
+    # we need to check if n is less than zero or if if it is zero
+    # use recursion until n is zero and add results up to give us the answer
+    #
+    # if n < 0:
+    #     return 0
+    # elif n == 0:
     #     return 1
-    # elif (n == 2):
-    #     return 2
     # else:
-    #     return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n - 1)
+    #     print(
+    #         f'1step: {eating_cookies(n - 1)},\n2step: {eating_cookies(n - 2)},\n3step: {eating_cookies(n - 3)}\n')
+    #     return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+
+    # pass
     eat = {1, 2, 3}
     cache = [0 for num in range(n+1)]
     cache[0] = 1
     for i in range(n+1):
         cache[i] += sum(cache[i - x] for x in eat if i - x > 0)
+        # print(f'cache at i({i}): {cache[i]}')
         cache[i] += 1 if i in eat else 0
+    # print(f'{cache}')
+    # print(f'last item: {cache[-1]}')
     return cache[-1]
 
 
--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,39 @@
 
 import sys
 
+
 def making_change(amount, denominations):
-  pass 
+    # pass
+    if amount < 0:
+        return 0
+    if amount == 0:
+        return 1
+    if not denominations:
+        return 0
+    return making_change(amount-denominations[0], denominations) + making_change(amount, denominations[1:])
+
+    # cache = [0 for num in range(amount+1)]
+    # cache[0] = 1
+    # for i in range(amount+1):
+    #     for x in denominations:
+    #         print(f'x: {x}')
+    #         if i - x > 0:
+    #             cache[i] += sum(cache[i - x])
+    #     if i in denominations:
+    #         cache[i] += 1
+    #     else:
+    #         0
+    # print(f'{cache}')
+    # return cache[-1]
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
